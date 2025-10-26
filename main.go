126331ee557f6e0c7ba5c140824844201276e356
pola <philipsola64@gmail.com>
2025-10-26 15:19:24 +0100

    Save local edits before rebase

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import "quad/piscine"
 
 func main() {
-	piscine.QuadA(5, 3)
-        println()
-	piscine.QuadB(5, 3)
-	println()
-	piscine.QuadC(5, 3)
-	println()
-	piscine.QuadD(5, 3)
-	println()
-	piscine.QuadE(5, 3)
+    piscine.QuadA(5, 3)
+    println()
+    piscine.QuadB(5, 3)
+    println()
+    piscine.QuadC(5, 3)
+    println()
+    piscine.QuadD(5, 3)
+    println()
+    piscine.QuadE(5, 3)
 }
+
